794db69f364f5d3f3493e3375c0900e615ab6fdc
NJM144 <injm144@gmail.com>
2025-07-11 19:35:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -602,4 +602,6 @@ def prediction():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Railway fixe automatiquement ce port
+    app.run(host='0.0.0.0', port=port)
