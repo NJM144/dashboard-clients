d7e13721a7f84000ef41cd21893876b1ce9a7b48
NJM144 <injm144@gmail.com>
2025-07-12 21:13:13 +0000

    feat: mise à jour requirements.txt et Dockerfile

--- app.py
+++ app.py
@@ -407,7 +407,7 @@ app = Flask(__name__)
 
 # Chargement du fichier de livraison (à adapter)
 df_livraisons = pd.read_csv("data/ListeTransfert_geocode (2) (1).csv", parse_dates=["DATE DU TRANSFERT"])
-df_livraisons.rename(columns={"DATE DU TRANSFERT": "date"}, inplace=True)  # pour standardiser
+df_livraisons.rename(columns={"DATE": "date"}, inplace=True)  # pour standardiser
 
 
 @app.route('/tournees')
